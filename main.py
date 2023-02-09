caf129645007d550201cbbf3fa29933a8ee46081
LuongTanDat <luongtandat.m0145@gmail.com>
2023-02-10 05:18:37 +0700

    feat: redirect root to swagger doc, add exported postman file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main.py
-from flask import Flask
+from flask import Flask, Blueprint, redirect, request, url_for
 from blueprints.basic_endpoints import blueprint as basic_endpoints
 from blueprints.jinja_endpoint import blueprint as jinja_endpoint
 from blueprints.documented_endpoints import blueprint as documented_endpoint
@@ -11,5 +11,16 @@ app.register_blueprint(basic_endpoints)
 app.register_blueprint(jinja_endpoint)
 app.register_blueprint(documented_endpoint)
 
+blueprint = Blueprint("", __name__, url_prefix="/")
+
+
+@blueprint.route("", methods=["GET"])
+def root():
+    if request.method == "GET":
+        return redirect("/documented_api/doc")
+
+
+app.register_blueprint(blueprint)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(host="0.0.0.0", port=8000, debug=True)
